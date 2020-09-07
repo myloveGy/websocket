5d40400809ab7ec2c31b594807f94f191e39b1f0
jinxing.liu <821901008@qq.com>
2020-09-07 15:29:36 +0800

    feat: 添加连接

--- main.go
+++ main.go
@@ -1,2 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		var conn, _ = upgrader.Upgrade(w, r, nil)
+		go func(conn *websocket.Conn) {
+			for {
+				mType, msg, _ := conn.ReadMessage()
+				fmt.Println("message", msg)
+				conn.WriteMessage(mType, msg)
+			}
+		}(conn)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":3000", nil)
+}
